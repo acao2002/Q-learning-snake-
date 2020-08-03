56a17bbad1c94b3d65c7ee31ae66b99c21df1274
acao2002 <xuanan12072002@gmail.com>
2020-08-03 10:26:09 -0400

    Create snake.py

---
+++ snake.py
@@ -0,0 +1,61 @@
+import pygame
+
+pygame.init()
+
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+
+
+dis=pygame.display.set_mode((800,600))
+pygame.display.update()
+pygame.display.set_caption('Snake game by Andrew')
+
+game_over=False
+
+x1 = 300
+y1 = 300
+ 
+x1_change = 0       
+y1_change = 0
+
+clock = pygame.time.Clock()
+
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x1_change = -10
+                y1_change = 0
+            elif event.key == pygame.K_RIGHT:
+                x1_change = 10
+                y1_change = 0
+            elif event.key == pygame.K_UP:
+                y1_change = -10
+                x1_change = 0
+            elif event.key == pygame.K_DOWN:
+                y1_change = 10
+                x1_change = 0
+ 
+    x1 += x1_change
+    y1 += y1_change
+    if x1>800:
+        x1 = 0;
+    if x1 < 0:
+        x1 = 800;
+    if y1>600:
+        y1 = 0;
+    if y1 < 0:
+        y1 = 600;       
+    dis.fill(white)
+    pygame.draw.rect(dis, black, [x1, y1, 10, 10])
+ 
+    pygame.display.update()
+ 
+    clock.tick(30)
+ 
+pygame.quit()
+quit()
