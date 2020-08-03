c92f02ef9edb6d104c07b180b4e1a755b3b73ba2
acao2002 <xuanan12072002@gmail.com>
2020-08-03 11:07:50 -0400

    nothing much

--- snake.py
+++ snake.py
@@ -1,4 +1,5 @@
 import pygame
+import random 
 
 pygame.init()
 
@@ -6,23 +7,30 @@ pygame.init()
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
+dis_height = 600
+dis_width = 800
 
-
-dis=pygame.display.set_mode((800,600))
+dis=pygame.display.set_mode((dis_width,dis_height))
 pygame.display.update()
 pygame.display.set_caption('Snake game by Andrew')
 
 game_over=False
 
-x1 = 300
-y1 = 300
+x1 = dis_width/2
+y1 = dis_height/2
  
 x1_change = 0       
 y1_change = 0
+snake_block = 10
 
+foodx = round(random.randrange(0, dis_width - 10) / 10.0) * 10.0
+foody = round(random.randrange(0, dis_width - 10) / 10.0) * 10.0
 clock = pygame.time.Clock()
 
+
+
 while not game_over:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
@@ -39,20 +47,30 @@ while not game_over:
             elif event.key == pygame.K_DOWN:
                 y1_change = 10
                 x1_change = 0
- 
+   
+
+    if x1>800:
+        x1 = 10;
+
+    if x1 < 0:
+        x1 = 790;
+
+    if y1>600:
+        y1 = 10;
+
+    if y1 < 0:
+        y1 = 790; 
+     
+     
     x1 += x1_change
     y1 += y1_change
-    if x1>800:
-        x1 = 0;
-    if x1 < 0:
-        x1 = 800;
-    if y1>600:
-        y1 = 0;
-    if y1 < 0:
-        y1 = 600;       
+    if x1 == foodx and y1 == foody:
+        foodx = round(random.randrange(10, dis_width - snake_block) / 10.0) * 10.0
+        foody = round(random.randrange(10, dis_width - snake_block) / 10.0) * 10.0
+
     dis.fill(white)
     pygame.draw.rect(dis, black, [x1, y1, 10, 10])
- 
+    pygame.draw.rect(dis, red, [foodx, foody, 10, 10])
     pygame.display.update()
  
     clock.tick(30)
